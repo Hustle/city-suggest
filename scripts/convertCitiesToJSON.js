5407128a6b2c4f1f319d40d66835b7f1a75b6294
Justin Higgins <justin@hustle.life>
2018-05-21 15:56:11 -0700

    use region name in cities conversion scripts

--- scripts/convertCitiesToJSON.js
+++ scripts/convertCitiesToJSON.js
@@ -3,23 +3,41 @@ const csv = require('csvtojson');
 const fs = require('fs');
 const CITIES_TXT_DIR = path.join(__dirname,'../','data','txt','cities');
 const CITIES_JSON_DIR = path.join(__dirname,'..','data','json','cities');
+const REGIONS_JSON_DIR = path.join(__dirname,'..','data','json','regions');
 const opts = {
-  headers: ['name','lat','lng','country','population','timezone'],
+  headers: ['name','lat','lng','country','regionCode','population','timezone'],
   delimiter: '	' // tab delimited
+};
+
+
+// NOTE: geonames.org gives 2-letter country codes. 3 is prettier
+const COUNTRY_CODE_MAP = {
+  'us': 'USA',
+  'ca': 'CAN'
 };
 
 function convertCSVToJSON(file) {
   csv(opts)
     .fromFile(path.join(CITIES_TXT_DIR, file))
     .then((cities) => {
-      const simpleCities = cities.map((city) => {
-        // remove unused columns
-        delete city.country;
-        delete city.population;
-        delete city.timezone;
-        return city;
+      const rootName = file.split('.txt')[0];
+      const fileName = rootName + '.json';
+      const regionMap = require(path.join(REGIONS_JSON_DIR, fileName));
+      const countryName = COUNTRY_CODE_MAP[rootName];
+      const simpleCities = [];
+
+      cities.forEach((city) => {
+        const simpleCity = {};
+        simpleCity.name = city.name;
+        simpleCity.displayName = `${city.name}, ${countryName}`;
+        const regionName = regionMap[city.regionCode];
+        if (regionName) {
+          simpleCity.displayName = `${city.name}, ${regionName}, ${countryName}`;
+        };
+        simpleCity.lat = city.lat;
+        simpleCity.lng = city.lng;
+        simpleCities.push(simpleCity);
       });
-      const fileName = file.replace('.txt', '.json');
       fs.writeFileSync(path.join(CITIES_JSON_DIR, fileName), JSON.stringify(simpleCities));
     })
 }
